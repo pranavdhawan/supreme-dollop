0137ca98c3cd059ac4db316b210b2b15b31943e4
Pranav Dhawan <dhawan.pranav8@gmail.com>
2020-01-17 22:29:55 +0530

    Update BirthdayCakeCandles.java

--- BirthdayCakeCandles.java
+++ BirthdayCakeCandles.java
@@ -1,4 +1,6 @@
-/* You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
+/* You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for
+each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones.
+Your task is to find out how many candles she can successfully blow out.
 */
 import java.io.*;
 import java.math.*;
